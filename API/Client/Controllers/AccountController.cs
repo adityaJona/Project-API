91ec246a1f30ef774f16b96aea95699046be1cd4
adityaJona <85984228+adityaJona@users.noreply.github.com>
2022-04-19 00:44:34 +0700

    new

---
+++ API/Client/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using API.Models;
+using API.ViewModels;
+using Client.Base;
+using Client.Models;
+using Client.Repositories.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class AccountController : BaseController<Account, AccountRepository, string>
+    {
+        private readonly AccountRepository _accountRepository;
+        public AccountController(AccountRepository repository) : base(repository)
+        {
+            this._accountRepository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
---
+++ API/Client/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+using Client.Base;
+using Client.Repositories.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class EmployeeController : BaseController<Employee, EmployeeRepository, string>
+    {
+        private readonly EmployeeRepository repository;
+        public EmployeeController(EmployeeRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetMasterAll()
+        {
+            var result = await repository.GetMasterData();
+            return Json(result);
+        }
+    }
+}
