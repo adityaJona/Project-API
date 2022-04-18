e5110ce3e8d43553a5cc576d71ed6ba156595f39
adityaJona <85984228+adityaJona@users.noreply.github.com>
2022-04-19 00:54:31 +0700

    new

---
+++ API/API/Controllers/EmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using API.Base;
+using API.Models;
+using API.Repository.Data;
+using API.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : BaseController<Employee, EmployeeRepository, string>
+    {
+        private readonly EmployeeRepository employeeRepository;
+        public IConfiguration configuration;
+        public EmployeesController(EmployeeRepository employeeRepository, IConfiguration configuration) : base(employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+            this.configuration = configuration;
+        }
+        //[Authorize(Roles = "Employee")]
+        [HttpGet("MasterData")]
+        public ActionResult GetMasterData()
+        {
+            var result = employeeRepository.GetMasterData();
+            return Ok(new { status = HttpStatusCode.OK, Result = result, Message = "DATA DITEMUKAN" });
+            //return Ok(result);
+        }
+
+        [HttpGet("TestCors")]
+        public ActionResult TestCors()
+        {
+            //return Ok("Test Cors Berhasil");
+            var result = employeeRepository.GetMasterData();
+            return Ok(result);
+        }
+
+        
+    }
+}
---
+++ API/API/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using API.Base;
+using API.Models;
+using API.Repository.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : BaseController<Role, RoleRepository, string>
+    {
+        private readonly RoleRepository roleRepository;
+        public RolesController(RoleRepository roleRepository) : base(roleRepository)
+        {
+            this.roleRepository = roleRepository;
+        }
+    }
+}
